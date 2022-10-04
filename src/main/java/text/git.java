7c4eb4293951529c3b6a074abde89bb67e917fcf
FrankZhang <FrankZhang.com>
2022-10-04 09:36:40 +0800

    hot-fix commit3

--- src/main/java/text/git.java
+++ src/main/java/text/git.java
@@ -2,8 +2,8 @@ package text;
 
 public class git {
     public static void main(String[] args) {
-        System.out.println("hello git");
-        System.out.println("hello git2");
+        System.out.println("hello git1");
+        System.out.println("hello git3");
         System.out.println("hello git4");
     }
 }
