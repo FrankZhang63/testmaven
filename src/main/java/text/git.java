922d1ecdd24e6c927225d26d9e4d10481ecf816d
FrankZhang <FrankZhang.com>
2022-10-04 11:30:36 +0800

    master commit3

--- src/main/java/text/git.java
+++ src/main/java/text/git.java
@@ -5,8 +5,5 @@ public class git {
         System.out.println("hello git");
         System.out.println("hello git2");
         System.out.println("hello git3");
-        System.out.println("hello git1");
-        System.out.println("hello git3");
-        System.out.println("hello git4");
     }
 }
